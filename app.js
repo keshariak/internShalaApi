2ec041e57fbfc2a2e8b6eef76de039c2dd6c7df8
keshariak <aryankeshari2003@gmail.com>
2024-09-04 23:50:03 +0530

    update

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ const cookieparser= require("cookie-parser");
 
  app.use(
     cors({
-        origin:"http://localhost:5173",
+        origin:"https://nauktilok-frontend-react.vercel.app",
     credentials:true,
    })
  )
